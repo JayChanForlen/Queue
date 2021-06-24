0f9a7b570ec6d06fb19d5f0a9ac05495ecc4c29b
wb_zhangyin <wb_zhangyin@meituan.com>
2021-06-24 17:09:18 +0800

    first Commit

---
+++ TestModel/src/main/java/Test.java
@@ -0,0 +1,16 @@
+/**
+ * @description:
+ * @author: JayChan
+ * @date: 2021/6/24
+ * @time: 3:18 下午
+ * Copyright (C) 2015 Meituan
+ * All rights reserved
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println(
+                "hello world!"
+        );
+    }
+}
